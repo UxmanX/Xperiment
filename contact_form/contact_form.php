0df7f2b03c5e2b81c7fe6a3475aa30e73a6c42e8
UxmanX <51507954+UxmanX@users.noreply.github.com>
2019-06-09 00:02:50 +0500

    Add files via upload

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -1,7 +1,7 @@
 <?php
 
 // configure
-$from = 'Contact form <yourmail@domain.com>';
+$from = 'Contact form <usman@xperimenting.cf>';
 $sendTo = 'your@mail.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
